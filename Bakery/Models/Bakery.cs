e2a71f41324363713b5d9b0b4bd2526376f87501
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-28 12:27:08 -0500

    pass test#1

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -7,7 +7,7 @@ namespace Bakery.Models
     {
         public static string FinalPrice(string input)
         {
-            int price = int.Parse(input);
+            string price = "Buy 2, get 1 free. A single loaf costs $5.";
             return price.ToString();
         }
     }
