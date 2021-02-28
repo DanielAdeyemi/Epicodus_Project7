c266cc555f000ac3916578c998ed0b4bd325a2b1
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-27 23:27:25 -0500

    setup test enviroment and  false pass test 0

---
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+
+    [TestMethod]
+    public void Test1()
+    {
+    }
+  }
+}
