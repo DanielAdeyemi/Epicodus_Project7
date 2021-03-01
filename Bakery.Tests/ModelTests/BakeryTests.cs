242e26239d69b27773496361f6ed99211b4bf96c
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-01 17:07:46 -0500

    fail test #4

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -12,22 +12,29 @@ namespace Scrabble.Tests
     [TestMethod]
     public void BreadConstractor_ReturnPrice_String()
     {
-      string price = Bread.FinalPrice("1");
+      string price = Bread.FinalPrice("");
       Assert.AreEqual("Buy 2, get 1 free. A single loaf costs $5.", price);
     }
 
     [TestMethod]
     public void PastryConstractor_ReturnPrice_String()
     {
-      string price = Pastry.FinalPrice("1");
+      string price = Pastry.FinalPrice("");
       Assert.AreEqual("Buy 1 for $2 or 3 for $5", price);
     }
 
     [TestMethod]
-    public void BreadPrice_ReturnPrice_String()
+    public void BreadPriceForOne_ReturnPrice_String()
     {
       string price = Bread.FinalPrice("1");
-      Assert.AreEqual("Toatal cost: $5", price);
+      Assert.AreEqual("Total cost: $5", price);
+    }
+
+    [TestMethod]
+    public void PastryPriceForOne_ReturnPrice_String()
+    {
+      string price = Pastry.FinalPrice("1");
+      Assert.AreEqual("Total cost: $2", price);
     }
   }
 }
