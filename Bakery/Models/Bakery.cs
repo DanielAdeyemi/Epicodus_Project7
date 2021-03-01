2fb0b9fa22e0aa418637010786a114712ed45fc1
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-01 17:15:09 -0500

    pass test #4

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 
 namespace Bakery.Models
 {
-    public class Bread
-    {
-        public static string FinalPrice(string input)
-        {
-            if (!String.IsNullOrEmpty(input)) {
-                string price = "Total cost: $5";
-                return price;
-            }
-            else
-            {
-
-            string price = "Buy 2, get 1 free. A single loaf costs $5.";
-            return price.ToString();
-            }
+  public class Bread
+  {
+    public static string FinalPrice(string input)
+      {
+        if (!String.IsNullOrEmpty(input)) 
+				{
+          string price = "Total cost: $5";
+          return price;
         }
-    }
-
-    public class Pastry
-    {
-        public static string FinalPrice(string input)
+        else
         {
-            string price = "Buy 1 for $2 or 3 for $5";
-            return price;
+    	    string price = "Buy 2, get 1 free. A single loaf costs $5.";
+          return price.ToString();
         }
+      }
+  }
+
+  public class Pastry
+  {
+    public static string FinalPrice(string input)
+    {
+    	if (!String.IsNullOrEmpty(input))
+      {
+        string price = "Total cost: $2";
+        return price;
+      }
+      else
+      {
+        string price = "Buy 1 for $2 or 3 for $5";
+        return price;
+      }
     }
+  }
 }
